f31173777a1580d533fa21d1adbf4999be535994
Junior Le Graphiste <juniorlegraphiste@gmail.com>
2026-09-03 00:57:21 +0100

    Fix bugs

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,9 @@
-import app from "../server/app";
-
-export default app
+export default function handler(
+  _req: Request,
+  res: any,
+) {
+  res.status(200).json({
+    ok: true,
+    message: "SiloCamp Vercel API fonctionne",
+  });
+}
